51b15e92f9c40a947eb935617575aceb192e416e
jmc529 <jmc529@vt.edu>
2020-04-11 02:35:24 -0400

    petridish rotation is acceptable. using OnMouseEnter vs current setup is to be seen

---
+++ Assets/Scripts/CursorChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorChange : MonoBehaviour
+{
+    public Texture2D defaultCursor;
+    public Texture2D openHandCursor;
+    public Texture2D closedHandCursor;
+    public Texture2D clickCursor;
+    private CursorMode mode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnMouseEnter");
+    }
+
+    public void Enter()
+    {
+        Cursor.SetCursor(openHandCursor, hotSpot, mode);
+    }
+
+    public void Exit()
+    {
+        Cursor.SetCursor(defaultCursor, hotSpot, mode);
+    }
+
+    public void Drag()
+    {
+        Cursor.SetCursor(closedHandCursor, hotSpot, mode);
+    }
+
+    public void Over()
+    {
+        Cursor.SetCursor(clickCursor, hotSpot, mode);
+    }
+}
---
+++ Assets/Scripts/DishRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DishRotation : MonoBehaviour
+{
+    private Quaternion targetAngle;
+    private Vector3 mouseLoc;
+
+    void Start()
+    {
+        targetAngle = transform.rotation;
+    }
+
+    public void Down()
+    {
+        mouseLoc = Input.mousePosition;
+    }
+
+    public void Drag()
+    {
+        Vector3 shift = mouseLoc - Input.mousePosition;
+        transform.Rotate(new Vector3(shift.y, shift.x, 0) * Time.deltaTime * 0.75f, Space.World);
+        mouseLoc = shift.magnitude > 300 ? Input.mousePosition : mouseLoc;
+    }
+
+    public void StartRelease()
+    {
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        while (transform.rotation != targetAngle)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetAngle, 0.25f);
+            yield return null;
+        }
+        yield return null;
+    }
+}
