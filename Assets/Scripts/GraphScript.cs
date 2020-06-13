3c24b8f281af7418ce764c73bf1629860ea6f74b
Ross W <rossw3@vt.edu>
2020-06-13 13:57:22 -0400

    Graph Updates

--- Assets/Scripts/GraphScript.cs
+++ Assets/Scripts/GraphScript.cs
@@ -13,7 +13,8 @@ public class GraphScript : MonoBehaviour
             graph.DataSource.StartBatch();  // start a new update batch
             graph.DataSource.ClearCategory("Player 1");  // clear the categories we have created in the inspector
             graph.DataSource.ClearCategory("Player 2");
-            for (int i = 0; i < 30; i++)
+
+            for (int i = 0; i < 5; i++)
             {
                 //add 30 random points , each with a category and an x,y value
                 graph.DataSource.AddPointToCategory("Player 1", Random.value * 10f, Random.value * 10f);
