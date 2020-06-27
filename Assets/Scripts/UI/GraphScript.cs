8de540250a44c79c3c806d66da6ab7806a1e83d4
jmc529 <jmc529@vt.edu>
2020-06-27 13:54:12 -0400

    Revert "hope this merge works"
    This reverts commit ea1c3e75566b159c8475639a21caf737f4c357f3, reversing
    changes made to 38a141e843cbce0c7bed4af620c97cb8e5bee3d9.

--- Assets/Scripts/UI/GraphScript.cs
+++ Assets/Scripts/UI/GraphScript.cs
@@ -1,22 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using ChartAndGraph;
-<<<<<<< HEAD:Assets/Scripts/GraphScript.cs
-using UnityEngine;
-
-public class GraphScript : MonoBehaviour
-{
-    void Start()
-    {
-        GraphChart graph = GetComponent<GraphChart>();
-        if (graph != null)
-        {
-            graph.DataSource.StartBatch();  // start a new update batch
-            graph.DataSource.ClearCategory("Player 1");  // clear the categories we have created in the inspector
-            graph.DataSource.ClearCategory("Player 2");
-
-            for (int i = 0; i < 5; i++)
-=======
 using UnityEngine;
 
 public class GraphScript : MonoBehaviour
@@ -30,7 +14,6 @@ public class GraphScript : MonoBehaviour
             graph.DataSource.ClearCategory("Player 1");  // clear the categories we have created in the inspector
             graph.DataSource.ClearCategory("Player 2");
             for (int i = 0; i < 30; i++)
->>>>>>> 38a141e843cbce0c7bed4af620c97cb8e5bee3d9:Assets/Scripts/UI/GraphScript.cs
             {
                 //add 30 random points , each with a category and an x,y value
                 graph.DataSource.AddPointToCategory("Player 1", Random.value * 10f, Random.value * 10f);
