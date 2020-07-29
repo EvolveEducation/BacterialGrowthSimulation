5b47afdfb6cf3da5b7995bae15f92b552e4425e5
Joe Conwell <jmc529@vt.edu>
2020-07-29 14:35:13 -0400

    current mess

--- Assets/Scripts/UI/SliderComponent.cs
+++ Assets/Scripts/UI/SliderComponent.cs
@@ -5,9 +5,13 @@ public class SliderComponent : MonoBehaviour
 {
     public Text textComponent;
     public Slider slider;
+    public bool whole;
 
     public void SetSliderValue()
     {
+        if (whole)
+        {
+        }
         textComponent.text = slider.value.ToString("0.00");
     }
 }
